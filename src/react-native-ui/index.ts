6e55665a9120cdc3d69b4b7b2b7a1f2a8a579e1b
Geoff Cramer <gcwill70@gmail.com>
2024-04-05 10:17:25 -0700

    export remaining components

--- src/react-native-ui/index.ts
+++ src/react-native-ui/index.ts
@@ -1,6 +1,15 @@
 // Button
 export { Button, ButtonProps } from "./components/Button/Button";
 
+// Loading Indicator
+export { LoadingIndicator, LoadingIndicatorProps } from "./components/LoadingIndicator/LoadingIndicator";
+
+// Modal
+export { Modal, ModalProps } from "./components/Modal/Modal";
+
+// Text
+export { Text, TextProps } from "./components/Text/Text";
+
 // TextButton
 export {
   TextButton,
@@ -8,5 +17,5 @@ export {
 } from "./components/TextButton/TextButton";
 
 // Theme
-export type { Theme } from "./theme/theme";
-export { ThemeProvider, useTheme } from "./theme/ThemeContext";
+export type { Theme } from "./Theme/theme";
+export { ThemeProvider, useTheme } from "./Theme/ThemeContext";
